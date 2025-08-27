4d053206c507f49ae91514cf63c82a01ba62b6e6
shlee0619 <shlee619@icloud.com>
2025-08-27 09:39:27 +0900

    chore: purge repository contents (keep history)

--- android/app/src/main/kotlin/com/example/studyassist/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.studyassist
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/features/study/domain/entities/study_item.dart
+++
@@ -1,5 +0,0 @@
-class StudyItem {
-  final String id;
-  final String title;
-  const StudyItem({required this.id, required this.title});
-}
